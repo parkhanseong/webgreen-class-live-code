96b02db80571f639a8ee6b7f26aaac9d91ad0f8e
hanseong park <hs88610@gmail.com>
2021-06-11 11:03:11 +0900

    jquery : basic laytou done

--- jquery/0610/basic-layout/js/script.js
+++ jquery/0610/basic-layout/js/script.js
@@ -7,23 +7,65 @@
 // 기본 세팅 2
 $(function(){
     // header scroll event
+    var $header = $('#header');
+    var $arrowUp = $('#arrowUp');
+
+    $(window).scroll(function(){
+        // number가 반환된다 = $를 붙이지 않는다 ( 개인 스타일 )
+        var scrollTop = $(window).scrollTop();
+        /* outerHeight는 border까지의 높이 포함 */
+        var headerHeight = $('#header').outerHeight();
+        var aboutHeight = $('#about').outerHeight();
+        // header가 감춰졌을때 fixed를 해서 따라오게 한다
+        // 스크롤된 값이 헤더의 높이보다 커지면
+
+        if( scrollTop > headerHeight ){
+            $header.addClass('fixed');
+        }else{
+            $header.removeClass('fixed');
+        }
+
+        // aboutSection을 지나치면 보여라
+        // scrollTop 값이 about section 높이보다 커지면
+        if( scrollTop > aboutHeight ){
+            $arrowUp.addClass('visible');
+        }else{
+            $arrowUp.removeClass('visible');
+        }        
+
+    })
 
     // toggle ele
     // gnb ele
     var $toggle = $('.toggle'),
-        $gnb = $('.gnb');
+        $gnb = $('.gnb'),
+        $dimmed = $('#dimmed');
 
     $($toggle).click(function(){
+        $dimmed.toggleClass('visible');
         $(this).toggleClass('active');
         $($gnb).toggleClass('visible');
     })
 
-    // gnb visible
-
     // dimmed effect
+    $dimmed.click(function(){
+        $(this).removeClass('visible');
+        $($gnb).removeClass('visible');
+        $toggle.removeClass('active');
+    })
 
     // gnb__item window scroll 
+    var $gnbMenu = $('.gnb__menu');
+    $gnbMenu.click(function(){
+        var target = $(this).attr('data-target');
+        // target은 클릭한 메뉴의 엘리먼트다
+        var $target = $(target);
+        var distance = $target.offset().top;
+        $('html, body').animate({scrollTop: distance});
+    })
 
     // arrow up window scroll
-
+    $arrowUp.click(function(){
+        $('html, body').animate({scrollTop: 0})
+    })
 })
